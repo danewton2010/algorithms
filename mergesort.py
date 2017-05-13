8616e9143d9dd2cf490612a57df04950cdabe342
danewton2010 <danewton@163.com>
2017-05-13 17:41:14 +0800

    mergesort

---
+++ mergesort.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+#  coding: utf-8
+
+def mergesort(seq):
+    mid = len(seq)//2
+    lft, rgt = seq[:mid], seq[mid:]
+    if len(lft) > 1 : lft = mergesort(lft)
+    if len(rgt) > 1 : rgt = mergesort(rgt)
+    res = []
+    while lft and rgt:
+        if lft[-1] >= rgt[-1]:
+            res.append(lft.pop())
+        else:
+            res.append(rgt.pop())
+    res.reverse()
+    return (lft or rgt) + res
+
+if __name__ == '__main__':
+    seq = [4, 3, 2, 1]
+    res = mergesort(seq)
+    print(res)
